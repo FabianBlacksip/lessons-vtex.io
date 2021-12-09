fc9e4508248712b538f560ebbe91908115e78bae
Jair Andres Diaz Puentes <jairdiaz@blacksip.com>
2021-12-09 17:52:23 -0500

    feat: ajuste tipo de dato

--- react/typings/storefront.d.ts
+++ react/typings/storefront.d.ts
@@ -4,10 +4,12 @@ declare global {
     interface StorefrontFunctionComponent<P = {}> extends FunctionComponent<P> {
         getSchema?(props: P): object;
         schema?: object;
+        defaultProps?: object
     }
 
     interface StorefrontComponent<P = {}, S = {}> extends Component<P, S> {
         getSchema?(props: P): object;
         schema: object;
+        defaultProps?: object
     }
 }
