fa8dabef131a433f65642ba02f7e32b86161f46f
FabianBlacksip <fabianvirguez@blacksip.com>
2022-08-03 16:04:32 -0500

    add custom componente slider layout v1

--- react/components/Example/Example.tsx
+++ react/components/Example/Example.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useCssHandles } from 'vtex.css-handles';
-import { ExampleProps } from '../../typings/example';
+import { ISliderItems, SliderCategory } from '../../typings/example';
+import { SliderLayout } from 'vtex.slider-layout';
+import { Image } from 'vtex.store-image';
 
 /**
  * declare todos los modificadores de clases que necesite su proyecto recuerde
  * que esto permitira que su proyectos sea configurable
  */
-const CSS_HANDLES = ['labelSeller'] as const;
+const CSS_HANDLES = [
+    'sliderItem',
+    'label_category'
+] as const;
 /**
  * declaracion del componente
  */
-const Example: StorefrontFunctionComponent<ExampleProps> = ({ label }) => {
+const Example: StorefrontFunctionComponent<ISliderItems> = ({ sliderCategory, itemsPerPage }) => {
     const handles = useCssHandles(CSS_HANDLES);
     return (
         <React.Fragment>
             <div className={`ml1 mt3 t-body`}>
-                <span className={`c-muted-2 ${handles.labelSeller}`}>
-                    {label}{' '}
-                </span>
+                <SliderLayout itemsPerPage={itemsPerPage}>
+                    {sliderCategory && sliderCategory?.map((_item: SliderCategory, index: number) => (
+                        <div className={`${handles.sliderItem}`} key={index}>
+                            <Image src={_item.image?.src} maxWidth={_item.image?.maxWidth} />
+                            <div className={`${handles.label_category} tc pv3`} >{_item.label}</div>
+                        </div>
+                    ))}
+                </SliderLayout>
             </div>
-        </React.Fragment>
+        </React.Fragment >
     );
 };
 
@@ -27,7 +37,69 @@ const Example: StorefrontFunctionComponent<ExampleProps> = ({ label }) => {
  * propiedades por defecto del componete
  */
 Example.defaultProps = {
-    label: 'Vendido por:',
+    itemsPerPage: {
+        desktop: 6,
+        tablet: 3,
+        phone: 3,
+    },
+    sliderCategory: [
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 1',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 2',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 3',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 4',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 5',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 6',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 7',
+        },
+        {
+            image: {
+                src: "https://alessaecuador.vtexassets.com/arquivos/electro.png?v=2",
+                maxWidth: "200px"
+            },
+            label: 'Category 8',
+        },
+    ]
 };
 
 /**
--- react/typings/example.d.ts
+++ react/typings/example.d.ts
@@ -1,3 +1,20 @@
-export interface ExampleProps {
+export interface ISliderItems {
+    sliderCategory: SliderCategory[];
+    itemsPerPage: ItemsPerPage;
+}
+
+export type SliderCategory = {
+    image: Image;
     label: string;
+}
+
+type ItemsPerPage = {
+    desktop: number | string;
+    tablet: number | string;
+    phone: number | string;
+}
+
+type Image = {
+    src: string;
+    maxWidth: string;
 }
